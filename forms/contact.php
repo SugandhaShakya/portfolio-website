47a907abd290b98cde36854b32e01c71379e43d4
Sugandha Man Shakya <shakyasugandha@gmail.com>
2023-03-22 16:10:52 +0545

    Initial commit

--- forms/contact.php
+++ forms/contact.php
@@ -6,8 +6,9 @@
   $to = "shakyasugandha@gmail.com";
   $location = "mail form website";
 
-  $headers = "From :" . $name . "\r\n" ;
-  $text = "You have received an email from ". $name."\r\n Email:" . "\r\n
+  $headers = "From :" . $name . "\r\n" . "CC: smshakya@sugandhamanshakya.com.np  "
+
+  $text = "You have received an email from ". $name."\r\n Email:" . $subject."\r\n Subject:" . "\r\n
   Message:" . $message;
 
   if($email!= NULL){
